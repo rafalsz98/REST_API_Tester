55ea102385db4eb329a9901bc0655d144025b41a
Paweł Tomczyk <pawerk4pl@gmail.com>
2021-01-30 17:12:51 +0100

    added DeleteTab

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,6 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "parametersmodel.h"
+#include "delete.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +12,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     qmlRegisterType<ParametersModel>("ParametersModel", 1, 0, "ParametersModel");
+    qmlRegisterType<Delete>("Delete", 1, 0, "Delete");
 
     QQmlApplicationEngine engine;
     engine.addImportPath("qrc:/components");
